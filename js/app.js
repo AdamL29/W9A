f61f31e934e367b1f6f3e43f426a452b00256f8f
Adam <adamlederer@live.ca>
2022-11-01 22:29:56 -0600

    Added json file with scenarios.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let userAge = 31;
+let isSubscribed = true;
+
+// To show about the person as a subcriber and of age
+
+if (userAge <= 30) {
+    console.log("Your are too young for this party.");
+} else if (isSubscribed == false){
+    console.log("Maybe this will be the one");
+} else if (isSubscribed == true){
+    console.log("Welcome to the party pal");
+} else {
+    console.log("Age is no problem for you.");
+}
